55ac7ea3133b6f24c8e1b043b3240df4dd42488b
atharvez <7atharvadesai@gmail.com>
2024-10-21 20:22:36 +0530

    modified:   components/Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -11,7 +11,7 @@ export default function Layout({children}) {
             <main>
                 <div>
                     {children}
-                    <Analytics />
+                    <Analytics/>
                 </div>
             </main>
         </div>
